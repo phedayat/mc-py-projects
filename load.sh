9bdd78a2e054024d08aabb24eb025c274a40567d
phedayat <phedayat@ucsd.edu>
2023-04-19 13:26:09 -0500

    Updated and modularized load.sh. Made all the stages functions and revamped logging.

--- load.sh
+++ load.sh
@@ -1,35 +1,56 @@
 #!/bin/bash
-device=$(rshell boards | grep -oh "/dev/\w*.\w*" | head -1)
+check_for_device(){
+	device=$(rshell boards | grep -oh "/dev/\w*.\w*" | head -1)
+	if [[ -c "$device" ]]; then
+		echo "[  DEV  ] Device found: $device"
+		export AMPY_PORT=$device
+	else
+		echo "[ERROR] Device not found. Check if the connection is secure or if you've loaded in bootloader mode"
+		exit 1
+	fi
+}
 
-if [[ -c "$device" ]]; then
-	echo "Device found: $device"
-	export AMPY_PORT=$device
-else
-	echo "[ERROR] Device not found. Check if the connection is secure or if you've loaded in bootloader mode"
-	exit 1
-fi
+collect_project_items(){
+	echo "[   1   ] Collecting project items from $projectRoot..."
+	projItems=$(find $projectRoot -maxdepth 1 -regex "$projectRoot/.*")
+	echo "[SUCCESS] Collected project items from $projectRoot..."
+}
 
-projectRoot=$1
-
-currentProjItems=$(find $projectRoot -maxdepth 1 -regex "$projectRoot/.*")
-echo "Collected project files and dirs from $projectRoot..."
-
-currentPicoFiles=$(ampy ls)
-echo "Collected files currently onboard the Pico..."
+collect_pico_items(){
+	echo "[   2   ] Collecting items currently onboard the Pico..."
+	picoItems=$(ampy ls)
+	echo "[SUCCESS] Collected items currently onboard the Pico"
+}
 
 # TODO Add if-statement here and corresponding option
 #	for `update`, meaning we don't need to clear
 #	the Pico
+remove_pico_items(){
+	echo "[   3   ] Removing all files from Pico..."
+	for i in $1; do
+		([[ -d "$i" ]] && ampy rmdir $i) || ampy rm $i
+	done
+	echo "[SUCCESS] Removed all files from Pico."
+}
 
-for i in $currentPicoFiles; do
-	([[ -d "$i" ]] && ampy rmdir $i) || ampy rm $i
-done
-echo "Removed all files from Pico..."
+loading_pico(){
+	echo "[   4   ] Loading all files from $projectRoot onboard the Pico"
+	for i in $1; do
+		ampy put $i
+	done
+	echo "[SUCCESS] Loaded all files from $projectRoot onboard the Pico"
+}
 
-for i in $currentProjItems; do
-	ampy put $i
-done
-echo "Put all files from $projectRoot onboard the Pico"
+check_for_device
 
-echo "Project loaded. You may now disconnect the board."
+projectRoot=$1
+projItems=()
+picoItems=()
 
+collect_project_items
+collect_pico_items
+
+remove_pico_items $picoItems
+loading_pico $projItems
+
+echo "[FINISHED] Project loaded. You may now disconnect the board."
