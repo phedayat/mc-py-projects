c4cf67d16b5206838198b64f0a13fd8ecff324d4
phedayat <phedayat@ucsd.edu>
2023-04-27 18:31:30 -0500

    Combined metdata and config when writing to metadata.json. Wrapped main in a try-except in case of keyboardinterrupt

--- projects/rigid-body-motion/main.py
+++ projects/rigid-body-motion/main.py
@@ -43,31 +43,36 @@ def run_unit(i, imu_dev, file, sleep_time):
     sleep(sleep_time)
 
 if __name__=="__main__":
-    # Prepare MPU6050 driver
-    i2c = I2C(0, sda=Pin(4), scl=Pin(5), freq=400000)
-    mpu = MPU6050(i2c)
+    try:
+        # Prepare MPU6050 driver
+        i2c = I2C(0, sda=Pin(4), scl=Pin(5), freq=400000)
+        mpu = MPU6050(i2c)
 
-    # Configure and prepare the run
-    config = get_config()
-    metadata = {
-        "run_id": "test_run_ticks2",
-        "fields": ["index", "time", "ax", "ay", "az", "gx", "gy", "gz"],
-    }
-    run_path = init_run(metadata)
-    run_csv_path = build_run_csv_path(run_path, metadata["run_id"])
+        # Configure and prepare the run
+        config = get_config()
+        metadata = {
+            "run_id": "run_1",
+            "fields": ["index", "time", "ax", "ay", "az", "gx", "gy", "gz"],
+        }
+        metadata.update(config)
+        run_path = init_run(metadata)
+        run_csv_path = build_run_csv_path(run_path, metadata["run_id"])
 
-    # Begin the run
-    led = Pin(25, Pin.OUT)
-    sleep_time = config["sleep_time"]
-    ticks = config["ticks"]
-    with open(run_csv_path, "a") as f:
-        led.value(1)
-        infty = ticks < 0
-        i = 0
-        while infty or i < ticks:
-            i += 1
-            run_unit(i, mpu, f, sleep_time)
-            
-    # Turn off LED and exit script
-    led.value(0)
-    exit(0)
+        # Begin the run
+        led = Pin(25, Pin.OUT)
+        sleep_time = config["sleep_time"]
+        ticks = config["ticks"]
+        with open(run_csv_path, "a") as f:
+            led.value(1)
+            infty = ticks < 0
+            i = 0
+            while infty or i < ticks:
+                run_unit(i, mpu, f, sleep_time)
+                i += 1
+                
+        # Turn off LED and exit script
+        led.value(0)
+        exit(0)
+    except KeyboardInterrupt:
+        led.value(0)
+        exit(0)
