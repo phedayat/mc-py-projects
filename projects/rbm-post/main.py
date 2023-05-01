8403886a7447e9469db5cd0ea47e17cd76eddda3
phedayat <phedayat@ucsd.edu>
2023-05-01 16:14:30 -0500

    Heavy update to main.py, now implementing arg parsing for choosing run, and using the DataExtractor

--- projects/rbm-post/main.py
+++ projects/rbm-post/main.py
@@ -1,5 +1,22 @@
+from collections import Counter
+from argparse import ArgumentParser
+
 from src.runs import RunObject
+from src.data_wrapper import DataExctrator
+
+ARGS = [
+    "--run-id",
+]
+
+def get_args():
+    parser = ArgumentParser(prog="RBM Post Processing")
+    [parser.add_argument(arg) for arg in ARGS]
+    return parser.parse_args()
 
 if __name__=="__main__":
-    run = RunObject("run_000")
-    assert run.config.get("version")
+    args = get_args()
+
+    run = RunObject(args.run_id)
+    info = DataExctrator(run.data.df, run.config)
+
+    info.summary()
