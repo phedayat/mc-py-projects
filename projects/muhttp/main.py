da8b048c8b118d778be39d02a70ab25adc35eb7f
phedayat <phedayat@ucsd.edu>
2023-09-21 00:12:27 -0500

    Updated test main file

--- projects/muhttp/main.py
+++ projects/muhttp/main.py
@@ -1,6 +1,15 @@
 from server.server_handler import Server
 
+
+
 if __name__=="__main__":
     server = Server(config_path="./config.json")
-    server.open_socket()
-    server.serve()
+    server.open_socket(
+        port=80,
+        max_requests=1,
+    )
+    server.serve({
+        "/": lambda x, y: "Hello",
+        "/app": lambda x, y: "Unlocked a new route!",
+        "/dev": lambda x, y: {"input": y},
+    })
